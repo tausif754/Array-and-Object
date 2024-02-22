af62fc6f5c5ee5e7fb02e8bf5a9690ed5413b917
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-22 15:30:54 +0530

    Question_1 completed

--- Question_1.js
+++ Question_1.js
@@ -5,4 +5,20 @@ if (!shopingCart.includes("Meat")) {
   shopingCart.unshift("Meat");
 }
 
+//Add 'sugar' at the end if not already added
+if (!shopingCart.includes("Sugar")) console.log(shopingCart);
+{
+  shopingCart.push("Sugar");
+}
+
+// Remove 'Honey' if allergic
+if (shopingCart.includes("Honey")) {
+  shopingCart.splice(shopingCart.indexOf("Honey"), 1);
+}
+
+// Modify 'Tea' to 'Green Tea'
+const teaIndex = shopingCart.indexOf("Tea");
+if (teaIndex !== -1) {
+  shopingCart[teaIndex] = "Green Tea";
+}
 console.log(shopingCart);
